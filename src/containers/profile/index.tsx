021d9c966d451fe2f9f8b7a92efdc18c98452d90
nightfury1111111111 <linjay0208@gmail.com>
2023-01-17 21:05:03 +0900

    sdf:

--- src/containers/profile/index.tsx
+++ src/containers/profile/index.tsx
@@ -3,8 +3,6 @@ import { Program, AnchorProvider, web3, utils } from "@project-serum/anchor";
 import { TOKEN_PROGRAM_ID, createAccount } from "@solana/spl-token";
 import * as anchor from "@project-serum/anchor";
 
-import { getOrCreateAssociatedTokenAccount } from "../../utils/transferSpl/getOrCreateAssociatedTokenAccount";
-
 import { Idl } from "@project-serum/anchor/dist/cjs/idl";
 import React, { Component, useState, useEffect } from "react";
 import { toast } from "react-toastify";
@@ -33,211 +31,13 @@ const Profile = () => {
   const { publicKey, wallet, signTransaction, signAllTransactions } =
     useWallet();
 
-  const [faqNum, setFaqNum] = useState(0);
-  const [stage, setStage] = useState(0);
-  const [currentEscrow, setCurrentEscrow] = useState(0);
-
-  const [escrowData, setEscrowData] = useState<EscrowData[]>([]);
-  const [totalValue, setTotalValue] = useState(0);
-  const [myStatus, setMyStatus] = useState("active");
-
-  const [currentMilestone, setCurrentMilestone] = useState(5);
-  const [description, setDescription] = useState("");
-  const [moderator, setModerator] = useState(constants.moderator);
-  const [amount, setAmount] = useState(500);
-  const [milestone1, setMilestone1] = useState("");
-  const [amount1, setAmount1] = useState(50);
-  const [milestone2, setMilestone2] = useState("");
-  const [amount2, setAmount2] = useState(150);
-  const [milestone3, setMilestone3] = useState("");
-  const [amount3, setAmount3] = useState(200);
-  const [milestone4, setMilestone4] = useState("");
-  const [amount4, setAmount4] = useState(50);
-  const [milestone5, setMilestone5] = useState("");
-  const [amount5, setAmount5] = useState(50);
-
-  const opts = {
-    preflightCommitment: "processed",
-  };
-
-  const getProvider = () => {
-    if (!wallet || !publicKey || !signTransaction || !signAllTransactions) {
-      return;
-    }
-    const signerWallet = {
-      publicKey: publicKey,
-      signTransaction: signTransaction,
-      signAllTransactions: signAllTransactions,
-    };
-
-    const provider = new AnchorProvider(connection, signerWallet, {
-      preflightCommitment: "recent",
-    });
-
-    return provider;
-  };
-
-  const createEscrow = async () => {
-    if (
-      currentMilestone > 0 &&
-      amount !== amount1 + amount2 + amount3 + amount4 + amount5
-    ) {
-      toast(
-        "Set the milestone payment correctly. Must be matched to total amount!"
-      );
-      return;
-    }
-    const provider = getProvider(); //checks & verify the dapp it can able to connect solana network
-    if (!provider || !publicKey || !signTransaction) return;
-    const program = new Program(idl as Idl, programID, provider);
-
-    const mint = new PublicKey(constants.mint);
-    const admin1 = new PublicKey(constants.admin1);
-    const admin2 = new PublicKey(constants.admin2);
-    const resolver = new PublicKey(moderator);
-
-    let admin2TokenAccount = await getOrCreateAssociatedTokenAccount(
-      connection,
-      publicKey,
-      mint,
-      admin2,
-      signTransaction
-    );
-
-    let initializerDepositTokenAccount =
-      await getOrCreateAssociatedTokenAccount(
-        connection,
-        publicKey,
-        mint,
-        publicKey,
-        signTransaction
-      );
-
-    const { adminSeed, stateSeed, vaultSeed, authoritySeed } = constants;
-    const randomSeed: anchor.BN = new anchor.BN(
-      Math.floor(Math.random() * 100000000)
-    );
-
-    // Derive PDAs: escrowStateKey, vaultKey, vaultAuthorityKey
-    const escrowStateKey = PublicKey.findProgramAddressSync(
-      [
-        Buffer.from(anchor.utils.bytes.utf8.encode(stateSeed)),
-        randomSeed.toArrayLike(Buffer, "le", 8),
-      ],
-      program.programId
-    )[0];
-    const vaultKey = PublicKey.findProgramAddressSync(
-      [
-        Buffer.from(anchor.utils.bytes.utf8.encode(vaultSeed)),
-        randomSeed.toArrayLike(Buffer, "le", 8),
-      ],
-      program.programId
-    )[0];
-
-    try {
-      //post request will verify the lib.json and using metadata address it will verify the programID and create the block in solana
-      const tx = await program.transaction.initialize(
-        randomSeed,
-        [
-          new anchor.BN(amount1),
-          new anchor.BN(amount2),
-          new anchor.BN(amount3),
-          new anchor.BN(amount4),
-          new anchor.BN(amount5),
-        ],
-        {
-          accounts: {
-            initializer: provider.wallet.publicKey,
-            vault: vaultKey,
-            admin1,
-            resolver,
-            admin2TokenAccount: admin2TokenAccount.address,
-            mint,
-            initializerDepositTokenAccount:
-              initializerDepositTokenAccount.address,
-            escrowState: escrowStateKey,
-            systemProgram: anchor.web3.SystemProgram.programId,
-            rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-            tokenProgram: TOKEN_PROGRAM_ID,
-          },
-          signers: [],
-        }
-      );
-      tx.feePayer = provider.wallet.publicKey;
-      tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-      const signedTx = await provider.wallet.signTransaction(tx);
-      const txId = await connection.sendRawTransaction(signedTx.serialize());
-      await connection.confirmTransaction(txId);
-      setStage(0);
-    } catch (err) {
-      // console.log(err.message);
-      console.log(err);
-    }
-  };
-
-  const getEscrow = async () => {
-    const provider = getProvider(); //checks & verify the dapp it can able to connect solana network
-    if (!provider || !publicKey || !signTransaction) return;
-    const program = new Program(idl as Idl, programID, provider);
-    try {
-      let tmpLockedval = 0;
-      await Promise.all(
-        (
-          await connection.getProgramAccounts(programID)
-        ).map(
-          async (
-            tx,
-            index //no need to write smartcontract to get the data, just pulling all transaction respective programID and showing to user
-          ) => {
-            const fetchData: any = await program.account.escrowState.fetch(
-              tx.pubkey
-            );
-            const newData = {
-              ...fetchData,
-              initializerAmount: [
-                Number(fetchData.initializerAmount[0]),
-                Number(fetchData.initializerAmount[1]),
-                Number(fetchData.initializerAmount[2]),
-                Number(fetchData.initializerAmount[3]),
-                Number(fetchData.initializerAmount[4]),
-              ],
-              randomSeed: Number(fetchData.randomSeed),
-            };
-            const lockedVal =
-              newData.initializerAmount[0] +
-              newData.initializerAmount[1] +
-              newData.initializerAmount[2] +
-              newData.initializerAmount[3] +
-              newData.initializerAmount[4];
-            tmpLockedval += lockedVal;
-            return {
-              ...newData,
-              pubkey: tx.pubkey.toString(),
-              active: lockedVal > 0 ? true : false,
-              index,
-            };
-          }
-        )
-      ).then((result) => {
-        setTotalValue(tmpLockedval);
-        setEscrowData(result);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   useEffect(() => {
-    if (stage === 0) getEscrow();
-  }, [wallet, publicKey, signTransaction, signAllTransactions, stage]);
-
-  useEffect(() => {
-    console.log(escrowData[currentEscrow]);
-  }, [currentEscrow]);
+    console.log(publicKey);
+  }, [publicKey]);
 
   return (
     <div className="bg-dashboard-backcolor min-h-[100vh] px-[49px] pb-[75px]">
@@ -281,7 +81,10 @@ const Profile = () => {
                 Disconnect
               </div>
               <div className="bg-[#1c262d] rounded-[9px] flex justify-center items-center text-[20px] leading-[23px] font-[700] py-[16px] px-[13px] cursor-pointer">
-                0xfddfdsg453gfregd432dfd4das
+                {publicKey &&
+                  publicKey?.toString().slice(0, 10) +
+                    "..." +
+                    publicKey?.toString().slice(-10)}
               </div>
             </div>
           </div>
